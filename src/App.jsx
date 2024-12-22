a0a58eb3fd646437b360d72c209036901d489896
DavidOlmos03 <juanda_201001@hotmail.com>
2024-12-22 13:59:22 -0500

    Organized styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Menu from './components/shared/Menu.jsx';
 import CheatSheet from './components/shared/CheatSheet.jsx';
 import Footer from './components/shared/Footer.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
-{/*import './styles/App.css'*/}
+import './styles/App.css';
 
 function App() {
   return (	
@@ -13,7 +13,7 @@ function App() {
       <div className="App">
       <Menu />	
       {/* Routes rendered here */}
-      <div className="shadow-lg m-md-5 p-md-5 m-2 p-2 rounded-3">
+      <div className="app-router shadow-lg m-md-5 p-md-5 m-2 p-2 rounded-3">
         <AppRouter />
       </div>
       <CheatSheet />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,6 +29,5 @@ const Home = () => {
     </section>
   );
 };
-
 export default Home;
 
